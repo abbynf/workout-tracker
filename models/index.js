832e89d493a4df6aa692419d98f9070de4c4d094
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-11-14 20:07:37 -0700

    reduced schemas needed, combined into one

--- models/index.js
+++ models/index.js
@@ -1,6 +1,26 @@
-// Gathers both models for Mongoose
+// Holds schema for the workout documents
+// Additional fields will be created by Mongoose as they are inserted
 
-module.exports = {
-    Cardio: require("./cardioSchema"),
-    User: require("./resistanceSchema")
-  };
+// DEPENDENCIES
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutChildSchema = new Schema({
+    typeOfExercise: String,
+    name: String,
+    duration: Number
+});
+
+const WorkoutSchema = new Schema({
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    },
+    // MAY NOT WORK IN ARRAY
+    exercises: [WorkoutChildSchema]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
