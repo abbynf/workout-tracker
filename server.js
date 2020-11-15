906cba22d4021cb3a6af75bab36e0e80e6ef668d
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-11-14 18:37:09 -0700

    added express server and connected to mongoosedb

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-// Server here
+// This file holds the express server and connects to Mongoose
+
+const express = require("express");
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test", { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("connected")
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
